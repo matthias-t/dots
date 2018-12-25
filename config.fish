16a338f47a795714e81324e85de8c463956c5c87
Matthias Totschnig <matthias@totschnig.org>
2018-12-25 11:42:18 +0100

    Clean up config.fish

--- config.fish
+++ config.fish
@@ -1,9 +1,11 @@
 set -x EDITOR micro
 set -x BROWSER firefox-developer-edition
+set -x TERM xst
 set -x TERMINAL xst
 set -x PATH $PATH /home/matthias/bin /home/matthias/.cargo/bin /home/matthias/code/go/bin
-set -x GOPATH /home/matthias/code/go
 set -x MAIL /home/matthias/documents/mail
+
+set -x GOPATH /home/matthias/code/go
 set -x PASSWORD_STORE_CHARACTER_SET '[:graph:]'
 set -gx QT_QPA_PLATFORMTHEME qt5ct
 
